d8b418a597dd2c0dd798cb7ba475037be85c6407
yrakemilio <yrakemilio@gmail.com>
2026-02-06 20:55:11 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
-// Tu URL de Apps Script (la que me acabas de pasar)
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyqTZY3jKziN-ZvLP-pdFBUKKwU0fA-Mrr6oXXV0pcG9EKw67mavvIY0SyIZbHpBixV/exec';
-
 const form = document.getElementById('form-encuesta');
 const btn = document.getElementById('btn-enviar');
 
 form.addEventListener('submit', e => {
-  e.preventDefault();
-  
-  // Cambiamos el estado del botón para que el usuario sepa que se está enviando
-  btn.disabled = true;
-  btn.innerText = "Enviando...";
+    e.preventDefault();
+    btn.disabled = true;
+    btn.innerText = "Enviando...";
 
-  // Enviamos los datos a la hoja de Google
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
     .then(response => {
-        alert("¡Muchas gracias! Tu opinión ha sido registrada con éxito.");
+        alert("¡Guardado con éxito en Cleanpisimo!");
+        form.reset();
         btn.disabled = false;
-        btn.innerText = "Enviar Encuesta";
-        form.reset(); // Limpia el formulario para un nuevo uso
+        btn.innerText = "Enviar mi opinión";
     })
     .catch(error => {
-        console.error('Error!', error.message);
-        alert("Hubo un error al enviar la encuesta. Por favor, inténtalo de nuevo.");
+        alert("Error al guardar. Revisa la conexión.");
         btn.disabled = false;
-        btn.innerText = "Enviar Encuesta";
+        btn.innerText = "Enviar mi opinión";
     });
 });
